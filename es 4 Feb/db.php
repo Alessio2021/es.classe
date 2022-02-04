2b9f7748691a32ae84b989d1df1b6a9ed2040de3
Alessio2021 <alessiobut1911@gmail.com>
2022-02-04 11:07:07 +0100

    first commit

---
+++ es 4 Feb/db.php
@@ -0,0 +1,170 @@
+<?php
+
+$cars = [
+    [
+        'ID' => 1,
+        'marca' => 'Ford',
+        'modello' => 'Ranger Raptor',
+        'cavalli' => null,
+        'immatricolazione' => 2020,
+        'cilindrata' => 1997,
+        'accessori' => "Telecamera per parcheggio assistito, Chiusura centralizzata, Sistema di chiamata d'emergenza, Chiusura centralizzata senza chiave, Apple CarPlay, CD",
+        'carburante' => 'Diesel',
+        'intestatari' => '',
+        'km' => '6600',
+        'trasmissione' => 'Automatico',
+        'carrozzeria' => 'SUV, Fuoristrada, Pick-up',
+        'trazione' => 'integrale',
+        'porte' => '4',
+        'localita' => 'Lugano',
+        'prezzo' => 48800.00,
+        'targa' => '',
+        'colore' => 'Blu, Azzurro',
+        'immagine' => 'https://prod.pictures.autoscout24.net/listing-images/f6e64a03-0317-487e-9307-1f37ac774716_06532003-9abf-42dd-a4f7-b37d431b58df.jpg/640x480.jpg',
+    ],
+    [
+        'ID' => 2,
+        'marca' => 'Ford',
+        'modello' => 'EcoSport',
+        'cavalli' => null,
+        'immatricolazione' => 2019,
+        'cilindrata' => 998,
+        'accessori' => "Cerchi in lega, Android Auto, Sistema di chiamata d'emergenza, Chiusura centralizzata senza chiave, Fari LED, Fendinebbia, Vivavoce, Volante in pelle",
+        'carburante' => 'Benzina',
+        'intestatari' => '',
+        'km' => '65500',
+        'trasmissione' => 'Manuale',
+        'carrozzeria' => 'SUV',
+        'trazione' => 'integrale',
+        'porte' => '5',
+        'localita' => 'Lugano',
+        'prezzo' => 12900.00,
+        'targa' => '',
+        'colore' => 'Bianco',
+        'immagine' => 'https://prod.pictures.autoscout24.net/listing-images/a8fe4dca-86a9-476e-912b-930123c11434_04dfea7f-402b-4832-a16d-7a6fbfbd71c5.jpg/640x480.jpg',
+    ],
+    [
+        'ID' => 3,
+        'marca' => 'Toyota',
+        'modello' => 'Yaris',
+        'cavalli' => null,
+        'immatricolazione' => 2017,
+        'cilindrata' => 1497,
+        'accessori' => "Fendinebbia, Controllo automatico clima, Fari LED, Chiusura centralizzata senza chiave, Telecamera per parcheggio assistito, Vivavoce, Volante in pelle",
+        'carburante' => 'Benzina, Elettrica',
+        'intestatari' => '',
+        'km' => '41631',
+        'trasmissione' => 'Manuale',
+        'carrozzeria' => 'Berlina',
+        'trazione' => 'Anteriore',
+        'porte' => '5',
+        'localita' => 'Lugano',
+        'prezzo' => 15220.00,
+        'targa' => '',
+        'colore' => 'Blue, Black',
+        'immagine' => 'https://img1.stcrm.it/images/20463013/1400x/toyota-yaris-hybrid-2017.jpg',
+    ],
+    [
+        'ID' => 4,
+        'marca' => 'Toyota',
+        'modello' => 'GT86',
+        'cavalli' => null,
+        'immatricolazione' => 2012,
+        'cilindrata' => 1998,
+        'accessori' => "Fendinebbia, Controllo automatico clima, Fari LED, Chiusura centralizzata senza chiave, Telecamera per parcheggio assistito, Vivavoce, Volante in pelle",
+        'carburante' => 'Benzina, Elettrica',
+        'intestatari' => '',
+        'km' => '55415',
+        'trasmissione' => 'Manuale',
+        'carrozzeria' => 'Coupe',
+        'trazione' => 'Posteriore',
+        'porte' => '3',
+        'localita' => 'Lugano',
+        'prezzo' => 24900.00,
+        'targa' => '',
+        'colore' => 'Red',
+        'immagine' => 'https://prod.pictures.autoscout24.net/listing-images/506ea2e0-b576-4112-80e7-d1712e53c17a_1eded1ed-be64-4017-bd75-cae936cb6503.jpg/640x480.jpg',
+    ],
+    [
+        'ID' => 5,
+        'marca' => 'Fiat',
+        'modello' => '500',
+        'cavalli' => null,
+        'immatricolazione' => 2019,
+        'cilindrata' => 1242,
+        'accessori' => "Fendinebbia, Controllo automatico clima, Airbag testa, Specchietti laterali elettrici, Sensori di parcheggio assistito posteriori, Chiusura centralizzata senza chiave, Telecamera per parcheggio assistito, Vivavoce, Volante in pelle",
+        'carburante' => 'Benzina',
+        'intestatari' => '',
+        'km' => '45843',
+        'trasmissione' => 'Manuale',
+        'carrozzeria' => 'Berlina',
+        'trazione' => 'Anteriore',
+        'porte' => '3',
+        'localita' => 'Lugano',
+        'prezzo' => 13950.00,
+        'targa' => '',
+        'colore' => 'Grigio Metallizzato',
+        'immagine' => 'https://prod.pictures.autoscout24.net/listing-images/4d9843e5-774c-4ba1-90c4-64b6ea2593cc_6a050a1f-06e7-4180-8121-3ad72907cf8e.jpg/640x480.jpg',
+    ],
+];
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach ($cars as $key => $car) { ?>
+        <div class="card">
+            <img src="<?php echo $car['immagine']; ?>" />
+            <div class="content">
+                <h1><?php echo $car['marca'] ?></h1>
+                <h2><?php echo $car['modello']; ?></h2>
+                <h3><?php echo $car['cavalli']; ?></h3>
+                <h3><?php echo $car['immatricolazione']; ?></h3>
+                <h3><?php echo $car['cilindrata']; ?></h3>
+                <h3><?php echo $car['accessori']; ?></h3>
+                <h3><?php echo $car['carburante']; ?></h3>
+                <h3><?php echo $car['intestatari']; ?></h3>
+                <h3><?php echo $car['km']; ?></h3>
+                <h3><?php echo $car['trasmissione']; ?></h3>
+                <h3><?php echo $car['carrozzeria']; ?></h3>
+                <h3><?php echo $car['trazione']; ?></h3>
+                <h3><?php echo $car['porte']; ?></h3>
+                <h3><?php echo $car['localita']; ?></h3>
+                <h3><?php echo $car['prezzo']; ?></h3>
+                <h3><?php echo $car['targa']; ?></h3>
+                <h3><?php echo $car['colore']; ?></h3>
+            </div>
+        </div>
+        <?php } ?>
+        // 'ID' => 1,
+        // 'marca' => 'Ford',
+        // 'modello' => 'Ranger Raptor',
+        // 'cavalli' => null,
+        // 'immatricolazione' => 2020,
+        // 'cilindrata' => 1997,
+        // 'accessori' => "Telecamera per parcheggio assistito, Chiusura centralizzata, Sistema di chiamata d'emergenza, Chiusura centralizzata senza chiave, Apple CarPlay, CD",
+        // 'carburante' => 'Diesel',
+        // 'intestatari' => '',
+        // 'km' => '6600',
+        // 'trasmissione' => 'Automatico',
+        // 'carrozzeria' => 'SUV, Fuoristrada, Pick-up',
+        // 'trazione' => 'integrale',
+        // 'porte' => '4',
+        // 'localita' => 'Lugano',
+        // 'prezzo' => 48800.00,
+        // 'targa' => '',
+        // 'colore' => 'Blu, Azzurro',
+        // 'immagine' =>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+$products = [
+    [
+        'nome' => 'Pollo Allo Spiedo',
+        'prezzo' => '4,99',
+        'immagine' => 'https://www.ricettedigusto.info/wp-content/uploads/2015/11/pollo-allo-spiedo-fatto-in-casa-1200x675.jpg',
+        'tipologia' => 'Gastronomia'
+    ],
+    [
+        'nome' => 'Pane',
+        'prezzo' => '0,99',
+        'immagine' => 'https://www.quellalucinanellacucina.it/wp-content/uploads/2021/01/pane-di-segale.jpg',
+        'tipologia' => 'Panetteria'
+    ],
+    [
+        'nome' => 'Salame',
+        'prezzo' => '3,99',
+        'immagine' => 'https://m.media-amazon.com/images/I/41fsD7Qe4gL._AC_.jpg',
+        'tipologia' => 'Gastronomia'
+    ],
+    [
+        'nome' => 'Pizza',
+        'prezzo' => '2,99',
+        'immagine' => 'https://d1e3z2jco40k3v.cloudfront.net/-/media/drog19/recipe-images/pizza-margherita-con-basilico_2000.jpg',
+        'tipologia' => 'Panetteria'
+    ],
+    [
+        'nome' => 'Rombo',
+        'prezzo' => '6,99',
+        'immagine' => 'https://www.melarossa.it/wp-content/uploads/2016/10/rombo-al-forno-1280x720.jpg',
+        'tipologia' => 'Pescheria'
+    ],
+    [
+        'nome' => 'Orata',
+        'prezzo' => '8,99',
+        'immagine' => 'https://wips.plug.it/cips/buonissimo.org/cms/2018/10/Orata-in-padella.jpg',
+        'tipologia' => 'Pescheria'
+    ],
+    [
+        'nome' => 'Gamberoni',
+        'prezzo' => '8,99',
+        'immagine' => 'https://www.pescheriamercatodelpesce.com/wp-content/uploads/2019/11/gambero-rosso.jpg',
+        'tipologia' => 'Pescheria'
+    ],
+    [
+        'nome' => 'Costolette di Agnello',
+        'prezzo' => '5,99',
+        'immagine' => 'https://thumbs.dreamstime.com/b/vista-superiore-delle-cotolette-crude-dell-agnello-con-i-rosmarini-ed-i-granelli-di-pepe-87814860.jpg',
+        'tipologia' => 'Macelleria'
+    ]
+    ];
+
+$cardsFiltered = $cards;
+
+if (isset($_GET['genre']) !== false) {
+  $genre = $_GET['genre'];
+  if ($genre === 'all') {
+    $cardsFiltered = $cards;
+  } else {
+    $cardsFiltered = [];
+    foreach ($cards as $card) {
+      if ($card['genre'] === $genre) {
+        $cardsFiltered[] = $card;
+      }
+    }
+  }
+} 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <form action="search.php" method="GET">
+        <input type="text" name="titolo">
+        <select name="genre" id="genre">
+            <option value="all">all</option>
+            <option value="Pescheria">Pescheria</option>
+            <option value="Gastronomia">Gastronomia</option>
+            <option value="Panetteria">Panetteria</option>
+            <option value="Macelleria">Macelleria</option>
+        </select>
+        <button>Cerca</button>
+    </form>
+    <div class="d-flex">
+        <?php foreach ($products as $product) { ?>
+            <div class="container">
+                <div class="image">
+                    <img src="<?= $product['immagine'] ?>" alt="">
+                </div>
+                <div class="text-center">
+                    <h1 class=""><?= $product['nome'] ?></h1>
+                    <h4 class="red">&euro; <?= $product['prezzo'] ?></h4>
+                    <h3 class=""><?= $product['tipologia'] ?></h3>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
